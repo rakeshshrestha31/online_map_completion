5791391830d16665f79f1980c8453d737db5392e
rakeshshrestha31 <rakeshsh31@gmail.com>
2018-08-02 09:34:16 -0700

    resnet VAE complete

--- models/resnet_vae.py
+++ models/resnet_vae.py
@@ -14,8 +14,11 @@ import torch
 from torch import nn
 from torch.autograd import Variable
 
+# standard library imports
+from collections import OrderedDict
+
 class ResnetVAE(nn.Module):
-    def __init__(self, resnet_version: str):
+    def __init__(self, resnet_version: str, latent_encoding_channels: int, skip_connection_type: str = 'concat'):
         super(ResnetVAE, self).__init__()
 
         self.resnet_versions = ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
@@ -24,21 +27,112 @@ class ResnetVAE(nn.Module):
             exit(1)
         self.resnet_version = resnet_version
 
-        self.encoder = getattr(resnet, self.resnet_version)
-        self.decoder = getattr(resnet_transpose, self.resnet_version)
+        self.encoder = getattr(resnet, self.resnet_version)()
+        self.decoder = getattr(resnet_transpose, self.resnet_version)(latent_encoding_channels, skip_connection_type)
+        
+        # ----------------------- Latent Mean ----------------------- #
+        self.latent_mean_encoder = OrderedDict([
+            ('latent_mean_conv', nn.Conv2d(
+                in_channels=512 * self.decoder.block.expansion,
+                out_channels=latent_encoding_channels,
+                kernel_size=(3, 3),
+                stride=(2, 2),
+                padding=1
+            )),
+
+            ('batch_norm_latent_mean_conv', nn.BatchNorm2d(num_features=latent_encoding_channels)),
+
+            ('activation_latent_mean_conv', nn.ReLU(inplace=True))
+        ])
+
+        # ----------------------- Latent logvariance ----------------------- #
+        self.latent_logvariance_encoder = OrderedDict([
+            ('latent_logvariance_conv', nn.Conv2d(
+                in_channels=512 * self.decoder.block.expansion,
+                out_channels=latent_encoding_channels,
+                kernel_size=(3, 3),
+                stride=(2, 2),
+                padding=1
+            )),
+
+            ('batch_norm_latent_logvariance_conv', nn.BatchNorm2d(num_features=latent_encoding_channels)),
+
+            ('activation_latent_logvariance_conv', nn.ReLU(inplace=True))
+        ])
+        
+        # Set the layers as attributes so that cuda stuffs can be applied
+        for layer_group in [self.latent_mean_encoder, self.latent_logvariance_encoder]:
+            for layer_name, layer in layer_group.items():
+                setattr(self, layer_name, layer)
+
 
     def forward(self, x):
         encoder_output = self.encoder(x)
         encoder_activations = self.encoder.layer_outputs
+        
+        # latent mean encoding
+        x = encoder_output
+        for layer_name, layer in self.latent_mean_encoder.items():
+            x = layer(x)
+        latent_mean_encoding = x
 
-        encoder_activations['input'] = encoder_output
+        # latent logvariance encoding
+        x = encoder_output
+        for layer_name, layer in self.latent_logvariance_encoder.items():
+            x = layer(x)
+        latent_logvariance_encoding = x
+        
+        encoder_activations['input'] = self.reparameterize(latent_mean_encoding, latent_logvariance_encoding) 
 
-        # TODO: latent encoding
         decoder_output = self.decoder(encoder_activations)
 
         return decoder_output
 
+    def reparameterize(self, mu: Variable, logvariance: Variable) -> Variable:
+        """
+        Randomly sample the latent vector given the mean and variance
+        """
+        std = logvariance.mul(0.5).exp_()
+        eps = Variable(std.data.new(std.size()).normal_())
+        return eps.mul(std).add_(mu)
+
 if __name__ == '__main__':
-    batch_size = 64
+    import torch
+    from torch.autograd import Variable
+    import utils.constants
+
+    import sys
+
+    from models import resnet
+    from utils.model_visualize import make_dot
+
+    batch_size = 4
+    latent_channels = 512
+
     input_size = (utils.constants.TARGET_HEIGHT, utils.constants.TARGET_WIDTH)
-    input = Variable(torch.FloatTensor(batch_size, 4, *input_size))
+
+    models = ['resnet18', 'resnet34', 'resnet50'] # , 'resnet101', 'resnet152']
+
+    for model_name in models:
+        print('model', model_name)
+        for skip_connection_type in ['none', 'concat', 'add']:
+            print('skip connection type', skip_connection_type)
+            model = ResnetVAE(model_name, latent_channels, skip_connection_type)
+            input_size = (utils.constants.TARGET_HEIGHT, utils.constants.TARGET_WIDTH)
+            input = Variable(torch.empty(batch_size, 4, *input_size).uniform_(0, 1))
+
+            for is_cuda in [False, True]:
+                print('cuda', is_cuda)
+                if is_cuda:
+                    input = input.cuda()
+                    model = model.cuda()
+                else:
+                    input = input.cpu()
+                    model = model.cpu()
+
+                output = model(input)
+                print('output: ', output.size())
+
+                dot = make_dot(output)
+                dot.render("/tmp/model", '.')
+                print('\n\n')
